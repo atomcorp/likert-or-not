4004aed8857f1200ff81878264c670b0fbc560c8
atomcorp <faotms@gmail.com>
2018-09-26 23:44:55 +0100

    eject, css modules

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import './App.css';
+import css from './App.css';
 import Page from './components/Page';
 
 import logo from './logo.svg';
@@ -8,10 +8,10 @@ class App extends React.Component {
   // public test =() => {}
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      <div className={css.container}>
+        <header className={css.header}>
+          <img src={logo} className={css.logo} alt="logo" />
+          <h1 className={css.title}>Welcome to React</h1>
         </header>
         <Page />
       </div>
