afd9a2ec475ce578859ace62ed42612ad710c5d7
atomcorp <thomasmaxwellsmith@gmail.com>
2018-10-15 23:43:47 +0100

    fix generated css names, force css names on dom

--- rollup.config.js
+++ rollup.config.js
@@ -1,39 +1,41 @@
-import typescript from "rollup-plugin-typescript2";
-import commonjs from "rollup-plugin-commonjs";
-import external from "rollup-plugin-peer-deps-external";
-import postcss from "rollup-plugin-postcss-modules";
-import resolve from "rollup-plugin-node-resolve";
-import url from "rollup-plugin-url";
+import typescript from 'rollup-plugin-typescript2';
+import commonjs from 'rollup-plugin-commonjs';
+import external from 'rollup-plugin-peer-deps-external';
+import postcss from 'rollup-plugin-postcss-modules';
+import resolve from 'rollup-plugin-node-resolve';
+import url from 'rollup-plugin-url';
 
-import pkg from "./package.json";
+import pkg from './package.json';
 
 export default {
-  input: "src/index.tsx",
+  input: 'src/index.tsx',
   output: [
     {
       file: pkg.main,
-      format: "cjs",
-      exports: "named",
-      sourcemap: true
+      format: 'cjs',
+      exports: 'named',
+      sourcemap: true,
     },
     {
       file: pkg.module,
-      format: "es",
-      exports: "named",
-      sourcemap: true
-    }
+      format: 'es',
+      exports: 'named',
+      sourcemap: true,
+    },
   ],
   plugins: [
     external(),
     postcss({
-      modules: true,
-      writeDefinitions: false
+      modules: {
+        generateScopedName: 'likert-or-not__[local]',
+      },
+      writeDefinitions: false,
     }),
     url(),
     resolve(),
     typescript({
-      rollupCommonJSResolveHack: true
+      rollupCommonJSResolveHack: true,
     }),
-    commonjs()
-  ]
+    commonjs(),
+  ],
 };
